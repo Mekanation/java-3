4013ccd3ae9a21058ed548c6ddea31a41d8793e3
Caleb Waters <cw@mekanation.com>
2020-06-29 13:46:32 -0500

    Initial setup

--- src/concurrency/Main.java
+++ src/concurrency/Main.java
@@ -4,5 +4,12 @@ public class Main {
    
     public static void main(String[] args) {
          //run all of your threads from this main class.
+        Reasoning r = new Reasoning();
+        r.run();
+        System.out.println(Color.getRandomColor() + "This is a test");
+
+        TeamTC1 codeConnoisseurs = new TeamTC1();
+        Thread t = new Thread(codeConnoisseurs);
+        t.start();
     }
 }
