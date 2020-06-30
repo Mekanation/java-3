4de669c0bd8555079def50dcb7c8bd3347334b01
Caleb Waters <cw@mekanation.com>
2020-06-30 17:09:43 -0500

    Finished animal refactor.

--- src/lambda_streams/Animals.java
+++ src/lambda_streams/Animals.java
@@ -1,149 +1,125 @@
 package lambda_streams;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-
 class Animals {
     //This class will be using streams.
-    static List<String> animals = Arrays.asList("peacoCK","rabbit","chiwawa","OranguTAN","vipeR","cobra","paNDa","bUffalo","DeeR","maLLard");
+    static List<String> animals = Arrays.asList("peacoCK", "rabbit", "chiwawa", "OranguTAN", "vipeR", "cobra", "paNDa",
+            "bUffalo", "DeeR", "maLLard");
 
     public static void main(String[] args) {
         System.out.println("original animals : " + animals);
 
-        //clean up the animals array by using the capsFirst() method. follow instructions in the method definition. 
-        List<String> cleaned = capsFirst(animals, false);
-        System.out.println("Cleaned: " + cleaned);
-       
-        //do not modify these addAnimal() method invocations
-        addAnimal("rEIndeeR");
-        addAnimal("Platypus");
-        addAnimal("frOg");
-        addAnimal("lEOpArD");
-        //---------------------------------------
+        // Lambda caps
+        lambdaCapsLower lc = (animaList, mutate) -> {
+            if (mutate) {
 
-        capsFirst(animals,true);
-        System.out.println("Default animals: " + animals);
+                Stream<String> myStream = animals.stream();
 
-        List<String> lowered = lowerFirst(animals,false);
-        System.out.println(lowered);
+                animals = myStream.map(String::toLowerCase)
+                        .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                        .collect(Collectors.toList());
+            } else {
+                return animaList.stream().map(String::toLowerCase)
+                        .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                        .collect(Collectors.toList());
+            }
 
-        flipAnimals(true);
+            return animals;
+        };
+        //Lambda lower
+        lambdaCapsLower ll = (animaList, mutate) -> {
+            if (mutate) {
+
+                Stream<String> myStream = animals.stream();
+
+                animals = myStream.map(String::toUpperCase)
+                        .map(name -> name.substring(0, 1).toLowerCase() + name.substring(1))
+                        .collect(Collectors.toList());
+            } else {
+                return animaList.stream().map(String::toUpperCase)
+                        .map(name -> name.substring(0, 1).toLowerCase() + name.substring(1))
+                        .collect(Collectors.toList());
+            }
+
+            return animaList;
+        };
+        //Lambda animal Add
+        lambdaStringAnimal aa = (animal) -> {
+            Stream<String> myStream = animals.stream();
+            Stream<String> myStream2 = Stream.concat(myStream, Stream.of(animal));
+
+            animals = myStream2.collect(Collectors.toList());
+            return animal;
+        };
+        //Lambda flip and sort
+        lambdaCapsLower fa = (animals, mutate) -> {
+            if (mutate) {
+                Collections.reverse(animals);
+
+                return animals;
+
+            } else {
+                Stream<String> myStream = animals.stream();
+
+                List<String> mylist = myStream.collect(Collectors.toList());
+                Collections.reverse(mylist);
+
+                return mylist;
+            }
+
+        };
+        lambdaCapsLower sa = (animals, mutate) -> {
+            Stream<String> myStream = animals.stream();
+
+            if (mutate) {
+
+                animals = myStream.sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+
+            } else {
+                return myStream.sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+            }
+
+            return animals;
+
+        };
+
+        addAnimal("rEIndeeR", aa);
+        addAnimal("Platypus", aa);
+        addAnimal("frOg" , aa);
+        addAnimal("lEOpArD", aa);
+
+        System.out.println(capsFirst(animals, true, lc));
         System.out.println(animals);
-
-        System.out.println("This is the non modified animals list: " + flipAnimals(false));
-        sortAnimals(true);
-        System.out.println("This is the modified animals list: " + animals);
-        System.out.println("This is the non modified animals list: " + sortAnimals(false));
-
-        //===========================Lambdas================================
-
-
-
-
-
+        System.out.println(lowerFirst(animals, true, ll));
+        System.out.println(animals);
+        System.out.println(flipAnimals(true, fa));
+        System.out.println(animals);
+        System.out.println(sortAnimals(true, sa));
+        System.out.println(animals);
     }
-
-
-
-    static List<String> capsFirst(List<String> animalList, boolean mutate) {
-        //clean up the animals list so that the first letter is capitalized, and all the other letters are lowercased. Use a stream to accomplish this task.
-        // Also, the 2nd parameter of this function is a boolean.  use this boolean 'flag' to determine whether or not to 'mutate'
-        // the original animals array stored as a static class field.
-        // if the flag is set to 'true', mutate the animals and return the animals out of the function.
-        // if it is false, create a copy of the animals, perform your stream operations on the copy, and return the copy of animals out of the function,
-        // WITHOUT modifying the original animals array.
-
-        return getStrings(animalList, mutate, animals.stream().map(Animals::stringSanitizer), animalList.stream().map(Animals::stringSanitizer));
-
-
+    interface lambdaCapsLower {
+       List<String> capsLower(List<String> animaList, boolean mutate);
     }
-
-    static void addAnimal(String animal) {
-        //add an animal to the animal list.
-
-        Stream<String> testStream = animals.stream();
-        Stream<String> newStream = Stream.concat(testStream, Stream.of(animal));
-
-        animals = newStream.collect(Collectors.toList());
-
+    interface lambdaStringAnimal{
+        String addAnimal(String animal);
+    }
+    static List<String> capsFirst(List<String> animaList, boolean mutate, lambdaCapsLower operation) {
+        return operation.capsLower(animaList, mutate);
+    }
+    static List<String> lowerFirst(List<String> animaList, boolean mutate, lambdaCapsLower operation) {
+        return operation.capsLower(animaList, mutate);
+    }
+    static String addAnimal(String animal, lambdaStringAnimal operation) {
+        return operation.addAnimal(animal);
     };
-
-    static List<String> lowerFirst(List<String> animalList, boolean mutate) {
-        //lowercase the first letter, and uppercase the rest of the letters, using streams.  Also, depending on the value of the boolean flag 'mutate', mutate the original animals list, or perform your stream operations on a 'copy' of the animals list.  return the list out of hte function in both cases.
-        return getStrings(animalList, mutate, animals.stream().map(Animals::stringSanitizerLowered), animalList.stream().map(Animals::stringSanitizerLowered));
+    static List<String> flipAnimals(boolean mutate, lambdaCapsLower operation) {
+        return operation.capsLower(animals,mutate);
     }
-
-    private static List<String> getStrings(List<String> animalList, boolean mutate, Stream<String> stringStream, Stream<String> stringStream2) {
-        Stream<String> newStream;
-        if(mutate) {
-
-            newStream = stringStream;
-
-
-            animals=newStream.collect(Collectors.toList());
-
-        }else{
-            newStream = stringStream2;
-
-
-            animalList=newStream.collect(Collectors.toList());
-
-        }
-        return animalList;
+    static List<String> sortAnimals(boolean mutate, lambdaCapsLower operation) {
+        return operation.capsLower(animals,mutate);
     }
-
-    static List<String> flipAnimals(boolean mutate) {
-        //reverse the order of the animals in the animal list.  If the boolean parameter is true, reverse the static animals array list by mutating the array.  if the mutate boolean is false, flip a 'copy' of the animals list, then return that list of flipped animals, WITHOUT mutating the static animals array. return the flipped list in both cases.
-        if(mutate){
-
-            Collections.reverse(animals);
-
-
-            return animals;
-        }
-        else{
-            Stream<String> animalStream = animals.stream();
-            List<String> list2 = animalStream.collect(Collectors.toList());
-            Collections.reverse(list2);
-            return list2;
-        }
-
-
-    }
-
-    static List<String> sortAnimals(boolean mutate) {
-    //sort the animals in alphabetical order.  If the boolean parameter is true, mutating the animals list.
-    // if the mutate boolean is false, sort a 'copy' of the animals list, then return that list of sorted animals,
-    // WITHOUT mutating the static animals array. return the sorted list in both cases.
-        if(mutate){
-            List<String> list = animals;
-            List<String> collect = list.stream().sorted().collect(Collectors.toList());
-            animals = collect;
-            return animals;
-        }else {
-            List<String> list = animals;
-            return list.stream().sorted().collect(Collectors.toList());
-        }
-
-    }
-
-    static String stringSanitizer(String s){
-        String s1 = s.toLowerCase();
-        String s2 = s1.substring(0,1).toUpperCase();
-        return s2 + s1.substring(1);
-
-    }
-
-    static String stringSanitizerLowered(String s){
-        String s1 = s.toUpperCase();
-        String s2 = s1.substring(0,1).toLowerCase();
-        return s2 + s1.substring(1);
-
-    }
-
 
 }
